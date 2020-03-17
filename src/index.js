aa0d936e77f71610730420daaf22cef35d0117df
Steven Wang <stevenwang2060@gmail.com>
2020-03-17 15:16:10 -0700

    Moved state from App.js to reducer. Created actions, implemented connect. Add and remove functions working. Changed min-height to max-height so the list doesn't keep stretching towards the bottom of the page. Reached MVP.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//redux imports
+import { Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+//reducer import
+import {reducer} from "./reducers/reducer";
+
+//styles
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//creating redux store
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    rootElement
+);
